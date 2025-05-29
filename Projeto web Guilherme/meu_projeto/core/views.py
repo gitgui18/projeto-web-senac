77d04a1463ed9f7c19dabbf0ab21e8ac810ebece
gitgui18 <guist0374@gmail.com>
2025-05-29 20:25:05 -0300

    Add files via upload

---
+++ Projeto web Guilherme/meu_projeto/core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'core/index.html')
+
+def how_to_play(request):
+    return render(request, 'core/howtoplay.html')
+
+def settings_view(request):  # ✅ novo nome
+    return render(request, 'core/settings.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def play(request):
+    return render(request, 'core/play.html')
+        
